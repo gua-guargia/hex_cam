ea1864e3707bef17bf00fc645b3d6f17b90839bd
Jiayi Zhang <60136318+gua-guargia@users.noreply.github.com>
2021-01-24 10:10:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ RFC_Model = pickle.load(open('./model/RF_model.pkl', 'rb'))
 
 def predict_single(img_file):
     'function to take image and return prediction'
-    test_image = cv2.imread(img_file)
+    test_image = cv2.imread(open_image(img_file))
     test_image = cv2.cvtColor(test_image, cv2.IMREAD_GRAYSCALE)
     test_image = cv2.resize(test_image, (224, 224))
     test_img = test_image.flatten().reshape(1, -1)
